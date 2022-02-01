0ab3a13f8641f2a1d203aa10849180020505568b
Ayush Garg <ayush181000@gmail.com>
2022-02-02 01:50:00 +0530

    parameter pollution

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -38,6 +39,20 @@ app.use(mongoSanitize());
 
 // Data sanitizers against XSS
 app.use(xss());
+
+// Prevent parameter pollution . Use it at the end because it clears req.query string
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'maxGroupSize',
+      'difficulty',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'price'
+    ]
+  })
+);
 
 // Serving static files
 app.use(express.static(`${__dirname}/public`));
