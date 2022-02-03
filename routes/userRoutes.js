e5078853d461da4f509ce961287e304cf684a8d6
Ayush Garg <ayush181000@gmail.com>
2022-02-04 00:50:54 +0530

    routes permission set

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,21 +4,21 @@ const authController = require('./../controllers/authController');
 
 const router = express.Router();
 
-router.patch(
-  '/updateMyPassword',
-  authController.protect,
-  authController.updatePassword
-);
-
-router.delete('/deleteMe', authController.protect, userController.deleteMe);
-
-router.patch('/updateMe', authController.protect, userController.updateMe);
-
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
-
 router.post('/forgotPassword', authController.forgotPassword);
 router.patch('/resetPassword/:token', authController.resetPassword);
+
+// all routes are protected under this middleware
+router.use(authController.protect);
+
+router.patch('/updateMyPassword', authController.updatePassword);
+
+router.get('/me', userController.getMe, userController.getUser);
+router.patch('/updateMe', userController.updateMe);
+router.delete('/deleteMe', userController.deleteMe);
+
+router.use(authController.restrictTo('admin'));
 
 router
   .route('/')
