1a2acdd6c3f686f28158303eb3cb3ea63daacb38
Ayush Garg <ayush181000@gmail.com>
2022-02-06 15:32:26 +0530

    .

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -94,6 +94,34 @@ exports.getMonthlyPlan = catchAsync(async (req, res, next) => {
   });
 });
 
+exports.getToursWithin = catchAsync(async (req, res, next) => {
+  const { distance, latlng, unit } = req.params;
+  const [lat, lng] = latlng.split(',');
+
+  const radius = unit === 'mi' ? distance / 3963.2 : distance / 6378.1;
+
+  if (!lat || !lng) {
+    next(
+      new AppError(
+        'Please provide lattitufe and longitude in format lat,lng',
+        400
+      )
+    );
+  }
+
+  // console.log(distance, lat, lng, unit);
+
+  const tours = await Tour.find({
+    startLocation: { $geoWithin: { $centerSphere: [[lng, lat], radius] } }
+  });
+
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: { data: tours }
+  });
+});
+
 // BUILD QUERY
 // 1A) Filtering
 // const queryObj = { ...req.query };
