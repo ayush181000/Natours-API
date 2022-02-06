5041d490259ce054ed43bab4a5434d3515b55fd5
Ayush Garg <ayush181000@gmail.com>
2022-02-06 14:59:34 +0530

    ratings average and duplicate rating

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Tour = require('../models/tourModel');
 
 const reviewSchema = new mongoose.Schema(
   {
@@ -29,6 +30,8 @@ const reviewSchema = new mongoose.Schema(
   }
 );
 
+reviewSchema.index({ tour: 1, user: 1 }, { unique: true });
+
 reviewSchema.pre(/^find/, function(next) {
   this
     // .populate({
@@ -40,6 +43,56 @@ reviewSchema.pre(/^find/, function(next) {
       select: 'name photo'
     });
   next();
+});
+
+reviewSchema.statics.calcAverageRatings = async function(tourId) {
+  // console.log(tourId);
+  // refers to the model directly
+  const stats = await this.aggregate([
+    {
+      $match: { tour: tourId }
+    },
+    {
+      $group: {
+        _id: '$tour',
+        nRating: { $sum: 1 },
+        avgRating: { $avg: '$rating' }
+      }
+    }
+  ]);
+
+  // console.log(stats);
+  if (stats.length > 0) {
+    await Tour.findByIdAndUpdate(tourId, {
+      ratingsQuantity: stats[0].nRating,
+      ratingsAverage: stats[0].avgRating
+    });
+  } else {
+    await Tour.findByIdAndUpdate(tourId, {
+      ratingsQuantity: 0,
+      ratingsAverage: 0
+    });
+  }
+};
+
+// post does not have access to next middleware
+reviewSchema.post('save', function() {
+  // this points to current review
+  // this.constructor is the model
+  this.constructor.calcAverageRatings(this.tour);
+});
+
+// findByIdAndUpdate
+// findByIdAndDelete
+reviewSchema.pre(/^findOneAnd/, async function(next) {
+  this.r = await this.clone().findOne();
+  console.log(this.r);
+  next();
+});
+
+reviewSchema.post(/^findOneAnd/, async function() {
+  // this.r = await this.findOne(); does NOT WORK here. as already executed
+  await this.r.constructor.calcAverageRatings(this.r.tour);
 });
 
 const Review = mongoose.model('Review', reviewSchema);
