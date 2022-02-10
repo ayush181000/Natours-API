0ae60b42f9e06afb7661263df0cccfc2a1fc38bc
Ayush Garg <ayush181000@gmail.com>
2022-02-10 16:50:57 +0530

    ,

--- utils/email.js
+++ utils/email.js
@@ -1,41 +1,71 @@
 const nodemailer = require('nodemailer');
+const pug = require('pug');
+const htmlToText = require('html-to-text');
 
-const sendEmail = async options => {
-  // 1) Create a transporter
-  //   const transporter = nodemailer.createTransport({
-  //     service: 'Gmail',
-  //     auth: {
-  //       user: process.env.EMAIL_USERNAME,
-  //       password: process.env.EMAIL_PASSWORD
-  //     }
-  //     // Activate in gmail "less secure app" option
-  //   });
+module.exports = class Email {
+  constructor(user, url) {
+    this.to = user.email;
+    this.firstName = user.name.split(' ')[0];
+    this.url = url;
+    this.from = `Natours Official <${process.env.EMAIL_FROM}>`;
+  }
 
-  const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    auth: {
-      user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD
+  newTransport() {
+    if (process.env.NODE_ENV === 'production') {
+      // sendgrid
+      return 1;
     }
-  });
 
-  //   console.log(transporter);
+    return nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD
+      }
+    });
+  }
 
-  // 2) Define the email options
-  const mailOptions = {
-    from: 'Ayush Garg <hello@ayush.io>',
-    to: options.email,
-    subject: options.subject,
-    text: options.message
-    // html:
-  };
+  async send(template, subject) {
+    // 1) Render HTML based on a pug template
+    const html = pug.renderFile(`${__dirname}/../views/email/${template}.pug`, {
+      firstName: this.firstName,
+      url: this.url,
+      subject
+    });
 
-  // 3) Actually send the email
+    // 2) Define email options
+    const mailOptions = {
+      from: this.from,
+      to: this.to,
+      subject,
+      html,
+      text: htmlToText.fromString(html)
+    };
 
-  // console.log(
-  await transporter.sendMail(mailOptions);
-  // );
+    // 3) Create a transport and send email
+    await this.newTransport().sendMail(mailOptions);
+  }
+
+  async sendWelcome() {
+    await this.send('welcome', 'welcome to the natours family');
+  }
 };
 
-module.exports = sendEmail;
+// const sendEmail = async options => {
+//   // 1) Create a transporter
+//   //   const transporter = nodemailer.createTransport({
+//   //     service: 'Gmail',
+//   //     auth: {
+//   //       user: process.env.EMAIL_USERNAME,
+//   //       password: process.env.EMAIL_PASSWORD
+//   //     }
+//   //     // Activate in gmail "less secure app" option
+//   //   });
+//   //   console.log(transporter);
+//   // 2) Define the email options
+//   // 3) Actually send the email
+//   // console.log(
+//   // await transporter.sendMail(mailOptions);
+//   // );
+// };
